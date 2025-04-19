62b93ed15b9de4dfe01af77efe8b1d0ef76459d0
paila sindhuja <sindhujapaila102@gmail.com>
2025-04-19 15:30:38 +0530

    add

--- script.js
+++ script.js
@@ -15,9 +15,21 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // 1. Get the value of the 'count' cookie
+  let count = parseInt(getCookie('count'));
   // 2. If the cookie exists, increment the value and update the cookie
+  if (!isNaN(count)){
+    count+=1
+  }else{
+    count=1
+  }
   // 3. If the cookie does not exist, create it and set the value to 1
+  setCookie('count',count,30);
   // 4. Display the count on the webpage
 
   // your code here
+  const display = document.createElement('div');
+  display.textContent = `🍪 You have visited this page ${count} time(s)!`;
+  display.style.fontSize = '20px';
+  display.style.marginTop = '20px';
+  document.body.appendChild(display);
 });
